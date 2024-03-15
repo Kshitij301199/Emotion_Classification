14d0b753e8dd79a01f015dd06776db78d6caa34f
Kshitij Kar <kshitij797@gmail.com>
2024-03-15 20:20:27 +0100

    Trained CNN and BiLSTM models

--- model/CNN_model.py
+++ model/CNN_model.py
@@ -7,22 +7,23 @@ def init_weights(m):
         m.bias.data.fill_(0.01)
         
 class CNNTextClassifier(nn.Module):
-    def __init__(self, 
-                 vocab_size:int = 52000, 
-                 embedding_dim:int = 16, 
-                 input_length:int = 100, 
-                 num_filters:int = 128, 
-                 kernel_size:int = 5, 
-                 hidden_units:int = 64, 
+    def __init__(self,
+                 vocab_size:int = 52000,
+                 embedding_dim:int = 64,
+                 input_length:int = 100,
+                 num_filters:int = 32,
+                 kernel_size:int = 3,
+                 hidden_units:int = 16,
                  num_classes:int = 6):
         super(CNNTextClassifier, self).__init__()
         self.embedding = nn.Embedding(vocab_size, embedding_dim)
         self.conv1d = nn.Conv1d(in_channels=embedding_dim, out_channels=num_filters, kernel_size=kernel_size)
         self.relu = nn.ReLU()
         self.maxpool = nn.MaxPool1d(kernel_size=input_length - kernel_size + 1)
-        self.fc1 = nn.Linear(num_filters, hidden_units)
-        self.fc2 = nn.Linear(hidden_units, num_classes)
-        self.softmax = nn.Softmax(dim=1)
+        self.dense = nn.Sequential(nn.Linear(num_filters, hidden_units),
+                                   nn.ReLU(),
+                                   nn.Linear(hidden_units, num_classes),
+                                   nn.Softmax(dim=1))
         
         self.apply(init_weights)
     
@@ -33,9 +34,5 @@ class CNNTextClassifier(nn.Module):
         x = self.relu(x)
         x = self.maxpool(x)
         x = x.squeeze(2)
-        x = self.fc1(x)
-        x = self.relu(x)
-        x = self.fc2(x)
-        x = self.softmax(x)
+        x = self.dense(x)
         return x
-        
